7c05bc4a2e249aa516d0b90f5bf6e7d579bd0fa6
Mark Timothy <marktimo256@gmail.com>
2026-07-24 07:57:58 +0300

    first commit

---
+++ Assets/javascript/main.js
@@ -0,0 +1,17 @@
+// Name Change Animation
+const logoName = document.querySelector(".logo-name");
+const firstName = "Talents College Mukono<span>Power Of Knowledge</span>";
+const secondName = "TASCOM International";
+setInterval(()=>{
+    logoName.classList.add("fade-out");
+    setTimeout(()=>{
+        if(logoName.innerHTML.trim() === firstName.trim())
+            logoName.innerHTML = secondName;
+        else
+            logoName.innerHTML = firstName;
+        logoName.classList.remove("fade-out");
+    }, 500);
+}, 5000);
+
+
+
---
+++ readMe.txt
@@ -0,0 +1,10 @@
+Challenges:
+   Difficult accessing the Heads of the school
+   School Policies tend to make life difficult
+
+Problem being solved by the project:[Creating Awareness]
+   The school is being known locally by community memebers, since it is a day and boarding school, i 
+believe it needs more customers in the neiboring districts of Mukono. For example Kampala, Jinja and the Day section 
+can be for the students coming from around Mukono while Boarding can be for those coming from far, as well as all.
+   
+  
